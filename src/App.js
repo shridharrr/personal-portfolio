c233acaf4a71b711ad78709091dfc3e89482de6f
shridharrr <shridharkatwe@gmail.com>
2024-01-04 11:51:30 +0400

    added

--- src/App.js
+++ src/App.js
@@ -7,12 +7,14 @@ import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
 import { Contact } from "./components/Contact";
 import { Footer } from "./components/Footer";
+import About from "./components/About";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Banner />
+      <About/>
       <Skills />
       <Projects />
       <Contact />
