4d4c0ae8468de3ac91b7560254eb29794c1d1338
shridhar <shridharkatwe7@gmail.com>
2024-01-07 12:11:52 +0530

    update

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -60,7 +60,9 @@ export const Projects = () => {
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
                 <h2 className="animation" data-aos = "zoom-in">Projects</h2>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                <p>
+                  Here are some of the projects such as single page applications, web apps, Portfolio designs, Invitation card designs etc.
+                </p>
                 <Tab.Container id="projects-tabs" defaultActiveKey="first">
                   <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
                     <Nav.Item>
